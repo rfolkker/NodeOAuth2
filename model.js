811c27be961d3d42b3b9b544fdd4a6e9d7db15f2
Roy Folkker <rfolkker@gmail.com>
2017-08-30 23:36:46 -0500

    Removing Database usage until I can get a better understanding of the options object

--- model.js
+++ model.js
@@ -25,65 +25,23 @@ var pool = new pg.Pool();
  */
 
 model.getAccessToken = function(bearerToken, callback) {
-    pool.username = "postgres";
-    pool.password = "Password";
-    client = pool.connect(connString);
-    client.query('SELECT access_token, client_id, expires, user_id FROM oauth_access_tokens ' +
-        'WHERE access_token = $1', [bearerToken],
-        function(err, result) {
-            if (err || !result.rowCount) return callback(err);
-            // This object will be exposed in req.oauth.token
-            // The user_id field will be exposed in req.user (req.user = { id: "..." }) however if
-            // an explicit user object is included (token.user, must include id) it will be exposed
-            // in req.user instead
-            var token = result.rows[0];
-            callback(null, {
-                accessToken: token.access_token,
-                clientId: token.client_id,
-                expires: token.expires,
-                userId: token.userId
-            });
-            done();
-        });
-    pool.end();
+    callback(null, {
+        accessToken: "token.access_token",
+        clientId: 12345,
+        expires: Date.now(),
+        userId: "RoyFolkker"
+    });
 };
 
 model.getClient = function(clientId, clientSecret, callback) {
-    pool.connect(function(err, client, done) {
-        if (err) return callback(err);
-
-        client.query('SELECT client_id, client_secret, redirect_uri FROM oauth_clients WHERE ' +
-            'client_id = $1', [clientId],
-            function(err, result) {
-                if (err || !result.rowCount) return callback(err);
-
-                var client = result.rows[0];
-
-                if (clientSecret !== null && client.client_secret !== clientSecret) return callback();
-
-                // This object will be exposed in req.oauth.client
-                callback(null, {
-                    clientId: client.client_id,
-                    clientSecret: client.client_secret
-                });
-                done();
-            });
+    callback(null, {
+        clientId: 12345,
+        clientSecret: "client.client_secret"
     });
-    pool.end();
 };
 
 model.getRefreshToken = function(bearerToken, callback) {
-    pool.connect(connString, function(err, client, done) {
-        if (err) return callback(err);
-        client.query('SELECT refresh_token, client_id, expires, user_id FROM oauth_refresh_tokens ' +
-            'WHERE refresh_token = $1', [bearerToken],
-            function(err, result) {
-                // The returned user_id will be exposed in req.user.id
-                callback(err, result.rowCount ? result.rows[0] : false);
-                done();
-            });
-    });
-    pool.end();
+    callback(false, true);
 };
 
 // This will very much depend on your setup, I wouldn't advise doing anything exactly like this but
@@ -98,43 +56,16 @@ model.grantTypeAllowed = function(clientId, grantType, callback) {
 };
 
 model.saveAccessToken = function(accessToken, clientId, expires, userId, callback) {
-    pool.connect(connString, function(err, client, done) {
-        if (err) return callback(err);
-        client.query('INSERT INTO oauth_access_tokens(access_token, client_id, user_id, expires) ' +
-            'VALUES ($1, $2, $3, $4)', [accessToken, clientId, userId, expires],
-            function(err, result) {
-                callback(err);
-                done();
-            });
-    });
-    pool.end();
+    callback(false);
 };
 
 model.saveRefreshToken = function(refreshToken, clientId, userId, expires, callback) {
-    pg.connect(connString, function(err, client, done) {
-        if (err) return callback(err);
-        client.query('INSERT INTO oauth_refresh_tokens(refresh_token, client_id, user_id, ' +
-            'expires) VALUES ($1, $2, $3, $4)', [refreshToken, clientId, userId, expires],
-            function(err, result) {
-                callback(err);
-                done();
-            });
-    });
-    pool.end();
+    callback(false);
 };
 
 /*
  * Required to support password grant type
  */
 model.getUser = function(username, password, callback) {
-    pool.connect(connString, function(err, client, done) {
-        if (err) return callback(err);
-        client.query('SELECT id FROM users WHERE username = $1 AND password = $2', [username,
-            password
-        ], function(err, result) {
-            callback(err, result.rowCount ? result.rows[0] : false);
-            done();
-        });
-    });
-    pool.end();
+    callback(false, true);
 };
